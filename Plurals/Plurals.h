db1f6d4ca855cec98c5cad07ff445ec0cd4489ec
Ike Mattice <ikemattice@icloud.com>
2020-01-03 16:18:37 -0800

    Initial Commit

---
+++ Plurals/Plurals.h
@@ -0,0 +1,19 @@
+//
+//  Plurals.h
+//  Plurals
+//
+//  Created by Ike Mattice on 1/3/20.
+//  Copyright © 2020 Ike Mattice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Plurals.
+FOUNDATION_EXPORT double PluralsVersionNumber;
+
+//! Project version string for Plurals.
+FOUNDATION_EXPORT const unsigned char PluralsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Plurals/PublicHeader.h>
+
+
